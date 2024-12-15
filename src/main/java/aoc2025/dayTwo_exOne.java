26cf3d5eb2b38b2187f38c455836cc3b072f2bfc
moerastrid@gmail.com <moerastrid@gmail.com>
2024-12-15 14:54:37 +0100

    not working yet..

--- src/main/java/aoc2025/dayTwo_exOne.java
+++ src/main/java/aoc2025/dayTwo_exOne.java
@@ -5,7 +5,10 @@ import java.util.List;
 import aoc2025.util.InputReader;
 
 public class dayTwo_exOne {
-	static private String path = "exampleTwo.txt";
+	static private String path = "inputTwo.txt";
+	static boolean increasing = false;
+	static boolean decreasing = false;
+
 	public static void main(String[] args) {
 
 		if (args.length == 1) {
@@ -27,8 +30,10 @@ public class dayTwo_exOne {
 		int answer = 0;
 
 		while (!strList.isEmpty()) {
+			increasing = false;
+			decreasing = false;
 			int returnval = issafe(strList.getFirst());
-			System.out.println("ret val: " + returnval);
+			System.out.println("ret val: " + returnval + "\n");
 			answer += returnval;
 			
 			strList.removeFirst();
@@ -38,14 +43,14 @@ public class dayTwo_exOne {
 
 	static private int issafe(String str) {
 		int safe = 1;
-		boolean increasing = false;
-		boolean decreasing = false;
 		String[] strArray = str.split(" ");
 
 		System.out.println("start: " + strArray[0] + " " + strArray[1]);
 		
 		boolean skipped = false;
 		for(int i = 0; safe == 1 && (i < strArray.length - 1); i++) {
+			boolean tempIncreasing = increasing;
+			boolean tempDecreasing = decreasing;
 			Integer current = Integer.parseInt(strArray[i]);
 			Integer next = Integer.parseInt(strArray[i + 1]);
 			Integer third = -1;
@@ -53,34 +58,37 @@ public class dayTwo_exOne {
 				third = Integer.parseInt(strArray[i + 2]);
 			}
 
-			if (smallissafe(current, next, increasing, decreasing) == 1)
-				return 1;
-			
-			if (third != -1 && skipped == false) {
+			if (smallissafe(current, next) == 1)
+				safe = 1;
+			else if (third != -1 && skipped == false) {
+				increasing = tempIncreasing;
+				decreasing = tempDecreasing;
 				skipped = true;
-				safe = smallissafe(current, third, increasing, decreasing);
+				safe = smallissafe(current, third);
 				i++;
+			} else {
+				safe = 0;
 			}
 		}
 		System.out.println(" safe: " + safe);
 		return safe;
 	}
 
-	static private int smallissafe(Integer current, Integer next, boolean increasing, boolean decreasing) {
+	static private int smallissafe(Integer current, Integer next) {
 		switch (current.compareTo(next)) {
 			case 0 :
-				System.out.println("same");
+				// System.out.println("same");
 				return 0;
 
 			case 1 :
-				System.out.println("decreasing");
+				// System.out.println("decreasing");
 				decreasing = true;
 				if (current - next > 3)
 					return 0;
 				break;
 
 			case -1 :
-				System.out.println("increasing");
+				// System.out.println("increasing");
 				increasing = true;
 				if (next - current > 3)
 					return 0;
@@ -94,3 +102,9 @@ public class dayTwo_exOne {
 		return 1;
 	}
 }
+
+
+// tries: 521 - too high!
+// tries: 416 - too high!
+// tries: 400 - too high!
+// tries: 237 - not right!
