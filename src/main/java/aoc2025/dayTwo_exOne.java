8dc324917a8c2710bdb2618f3e2961a18e251a33
moerastrid@gmail.com <moerastrid@gmail.com>
2024-12-15 04:26:57 +0100

    at the airport

--- src/main/java/aoc2025/dayTwo_exOne.java
+++ src/main/java/aoc2025/dayTwo_exOne.java
@@ -1,19 +1,46 @@
 package aoc2025;
 
+import java.util.List;
+
 import aoc2025.util.InputReader;
 
 public class dayTwo_exOne {
-	static private String path = "example.txt";
-
+	static private String path = "exampleTwo.txt";
 	public static void main(String[] args) {
 
 		if (args.length == 1) {
 			path = args[0];
 		} else {
-			System.out.println("INFO - Please provide input file! Now using example input (example.txt)");
+			System.out.println("INFO - Please provide input file! Now using example input (exampleTwo.txt)");
 		}
 
 		InputReader inputReader = new InputReader(path);
-		inputReader.convertFileToIntegerList();
+		List<String> input = inputReader.convertFileToStringList();
+		// String[] inputArray = input.split(" ");
+		System.out.println(input);
+
+		int answer = calculate(input);
+		System.out.println("THe ansWer: " + answer);
+	}
+
+	static private int calculate(List<String> strList) {
+		int answer = 0;
+
+		while (!strList.isEmpty()) {
+			answer += issafe(strList.getFirst());
+			strList.removeFirst();
+		}
+
+		return answer;
+	}
+
+	static private int issafe(String str) {
+		int safe = 1;
+		String[] strArray = str.split(" ");
+		
+		for(int i = 0; i < strArray.length; i++) {
+			System.out.println("strArray[" + i + "]: " + strArray[i]);
+		}
+		return safe;
 	}
 }
--- src/main/java/aoc2025/util/InputReader.java
+++ src/main/java/aoc2025/util/InputReader.java
@@ -8,8 +8,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InputReader {
-	private File file;
-	private String path;
+	final private File file;
+	final private String path;
+	private String str;
 	private BufferedReader br;
 
 	public InputReader(String _path) {
@@ -17,14 +18,31 @@ public class InputReader {
 		this.file = new File(path);
 	}
 
-	public List<Integer> convertFileToIntegerList() {
-		List<Integer> inputList = new ArrayList<>();
+	// public List<Integer> convertFileToIntegerList() {
+	// 	List<Integer> inputList = new ArrayList<>();
+
+	// 	try {
+	// 		br = new BufferedReader(new FileReader(file));
+	// 		while((str = br.readLine()) != null) {
+	// 			inputList.add(Integer.parseInt(str));
+	// 		}
+	// 	} catch (IOException error) {
+	// 		error.printStackTrace();
+	// 	}
+	// 	return inputList;
+	// }
+
+	public List<String> convertFileToStringList() {
+		List<String> stringList = new ArrayList<>();
 
 		try {
 			br = new BufferedReader(new FileReader(file));
+			while((str = br.readLine()) != null) {
+				stringList.add(str);
+			}
 		} catch (IOException error) {
+			error.printStackTrace();
 		}
-
-		return inputList;
+		return stringList;
 	}
 }
